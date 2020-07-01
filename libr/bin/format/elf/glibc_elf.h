23e8dcdad5b1f42de5e072e8aa97f108a93965b6
Jules Maselbas <jmaselbas@kalray.eu>
2020-07-01 17:11:35 +0200

    kvx: Add EM_KVX auto detect on elfid

--- libr/bin/format/elf/glibc_elf.h
+++ libr/bin/format/elf/glibc_elf.h
@@ -362,7 +362,9 @@ typedef struct
 #define EM_BPF		247	/* Linux BPF -- in-kernel virtual machine */
 #define EM_CSKY		252     /* C-SKY */
 
-#define EM_NUM		253
+#define EM_KVX		256	/* Kalray VLIW core of the MPPA processor family */
+
+#define EM_NUM		257
 
 /* Old spellings/synonyms.  */
 
